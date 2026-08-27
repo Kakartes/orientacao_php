9487c564d32511edb2f4c3afebcacfbb570fc01b
Rex <castro.karla.2301@gmail.com>
2026-08-27 13:49:41 -0300

    Add files via upload

---
+++ exercicio.REXphp.27ago/Modelo/circulo.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once("IFormaGeometrica.php");
+
+class Circulo implements IFormaGeometrica {
+
+    private int $raio;
+
+    public function getArea() {
+        return 3.14 * $this->raio * $this->raio;
+    }
+
+    public function getDesenho() {
+        $desenho = 
+        "   xxxxxxxxx\n" .   
+        " xxx       xxx\n" .
+        "xx           xx\n" .
+        "x             x\n" .
+        "x             x\n" .
+        "x             x\n" .
+        "xx            x\n" .
+        " xxx        xxx\n" .
+        "   xxxxxxxxxx\n";
+        return $desenho;
+    }
+
+   
+    public function getRaio(): int
+    {
+        return $this->raio;
+    }
+
+    public function setRaio(int $raio): self
+    {
+        $this->raio = $raio;
+
+        return $this;
+    }
+}
